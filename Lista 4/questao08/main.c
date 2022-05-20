3c46bba205598654abca5fd1f25ac343a8f68da5
Júnior Veras <iramarjunhorveras@gmail.com>
2022-05-20 12:39:09 -0300

    Lista 4 (Inicio)

---
+++ Lista 4/questao08/main.c
@@ -0,0 +1,13 @@
+/*
+8. Crie um programa que lê 6 valores inteiros e, em seguida, mostre na tela os valores lidos na
+ordem inversa
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}

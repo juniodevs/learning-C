38ff003c92ed2813fe920d18ff3ee518f812555d
Júnior Veras <iramarjunhorveras@gmail.com>
2022-05-23 09:56:51 -0300

    Lista 5 (Up)

--- Lista 5/questao17/main.c
+++ Lista 5/questao17/main.c
@@ -1,3 +1,12 @@
+/*
+17. Faça um programa para determinar a próxima jogada em um Jogo da Velha. Assumir que o
+tabuleiro é representado por uma matriz de 3 x 3, onde cada posição representa uma das casas do
+tabuleiro. A matriz pode conter os seguintes valores -1, 0, 1 representando respectivamente uma
+casa contendo uma peça minha (-1), uma casa vazia do tabuleiro (0), e uma casa contendo uma
+peça do meu oponente (1).
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>

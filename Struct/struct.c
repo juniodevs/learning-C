f93302a470037ffd6c50ed8972d273d387ee930f
Júnior Veras <iramarjunhorveras@gmail.com>
2022-06-27 12:10:12 -0300

    Adição de Struct.c

---
+++ Struct/struct.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    
+    struct ficha_aluno //tipo de dado
+    {
+        char nome[40];
+        int numero;
+        float nota;
+    }; //definição da struct
+
+    struct ficha_aluno aluno;
+
+    printf("\n CADASTRO DE ALUNO \n");
+
+    printf("Nome do aluno ...:");
+    fgets(aluno.nome, 40 ,stdin);
+
+    printf("\n Digite o numero do aluno: ");
+    scanf("%d", &aluno.numero);
+
+    printf("\n Informe a nota do Aluno: ");
+    scanf("%f", &aluno.nota);
+
+    printf("\n\n LENDO DADOS DA STRUCT \n\n");
+    printf("Nome: %s", aluno.nome);
+    printf("Numero: %d", aluno.numero);
+    printf("\nNota: %2.f", aluno.nota);
+
+    return 0;
+}
